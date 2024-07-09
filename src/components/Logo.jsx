855fa74a8b636c3827e50ad856bfa6b4afe9a2a7
Vikram13shri <vikrams13122004@gmail.com>
2024-07-09 16:55:22 +0530

    commit changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 function Logo({width,height="60px"}) {
   return (
     <div>
-      <img src="../../src/assets/PostPitLogo_1.svg" width={width} height={height} alt="" />
+      {/* <img src="../../src/assets/PostPitLogo_1.svg" width={width} height={height} alt="" /> */}
+      Logo
     </div>
   )
 }
